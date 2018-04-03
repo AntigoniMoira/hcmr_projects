a7b8e26c05120fedd9b37a97a134a9f3d8b2e5c8
AntigoniMoira <antmoira@gmail.com>
2018-04-03 10:49:47 +0300

    first 2 api requests

--- api/urls.py
+++ api/urls.py
@@ -4,10 +4,15 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^help', views.help, name='help'),
-    #url(r'^(?P<platform>\w+)/',views.platforms_data,name='platforms_data'),
     url(r'^platforms/', views.PlatformList.as_view(),name='platform_list'),
     url(r'^institutions/', views.InstitutionList.as_view(),name='institution_list'),
     url(r'^parameters/', views.ParameterList.as_view(),name='parameter_list'),
     url(r'^(?P<platform>\w+)/', views.DataList.as_view(),name='data_list'),
+    # excel_service urls
+    url(r'^poseidon_platforms_with_measurements_between',  
+            views.poseidon_platforms_with_measurements_between, name='poseidon_platforms_with_measurements_between'),
+    url(r'^poseidon_platform_parameters_with_measurements_between',  
+            views.poseidon_platform_parameters_with_measurements_between, name='poseidon_platform_parameters_with_measurements_between'),
+            
     
 ]
